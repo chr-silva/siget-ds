cf6dc39160804511c34895bf906bd5dc188bb74c
Chris Silva <chr.ivansilva@gmail.com>
2025-12-22 20:26:38 -0300

    Commit inicial do projeto

---
+++ js/calendar.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function () {
+  var calendarEl = document.getElementById('calendar');
+
+  var calendar = new FullCalendar.Calendar(calendarEl, {
+    locale: 'pt-br',
+    initialView: 'dayGridMonth',
+    // aqui dentro vão só as configurações do FullCalendar
+  });
+
+
+    //carrega o json de dias especiais
+    fetch("../js/events.json") //dar fetch SEMPRE no caminho correto relativo ao html
+    .then(res => res.json())
+    .then(eventos => calendar.addEventSource(eventos)).catch( err => {
+        console.error("Erro:", err);
+        calendar.render();
+    });
+
+    calendar.render(); //depois renderiza o calendario
+});
