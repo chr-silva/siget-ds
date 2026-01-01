bd6e65301b7a5968924a1694ea83c7c1b3feb726
Chris Silva <chr.ivansilva@gmail.com>
2026-01-01 17:17:40 -0300

    refactor: torna guard assíncrono com controle de auth e verificação de email.

--- js/guard.js
+++ js/guard.js
@@ -1,11 +1,38 @@
 // guard.js
 import { auth } from "./firebase.js";
-import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
-console.log("guard.js - OK");
+import { onAuthStateChanged } from
+  "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 
+let authResolvida = false;
+let authPromise = null;
+let emailVerificado = true;
 
-onAuthStateChanged(auth, (user) => {
-  if (!user) {
-    window.location.href = "/pages/siget/login/";
+export function isEmailVerificado() {
+  return emailVerificado;
+}
+
+export function esperarAuth() {
+  if (authResolvida) {
+    return Promise.resolve(auth.currentUser);
   }
-});
+
+  if (!authPromise) {
+    authPromise = new Promise((resolve) => {
+      onAuthStateChanged(auth, async (user) => {
+        authResolvida = true;
+
+        if (!user) {
+          window.location.href = "/pages/siget/login/";
+          return;
+        }
+
+        await user.reload();
+        emailVerificado = user.emailVerified;
+
+        resolve(user);
+      });
+    });
+  }
+
+  return authPromise;
+}
